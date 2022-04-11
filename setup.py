d5412be7b8b8c50cf917cf181f5f29332bfdf6cb
mex312 <mex3122020@gmail.com>
2022-04-11 22:40:44 +0300

    kinda stable

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from setuptools import setup
 
 setup(
     name='EveryBotModulePack',
-    version='0.1b2',
-    description='Module package for EveryBot',
+    version='0.1b3',
     packages=['EveryBot'],
+    url='',
+    license='',
+    author='mex312',
+    author_email='mex3122020@gmail.com',
+    description='Pack for creating modules for EveryBot',
+    requires=['pyTelegramBotApi'],
     install_requires=['pyTelegramBotApi']
 )
