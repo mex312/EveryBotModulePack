a97de57bf54860919a4204da3d717585cdbafc36
mex312 <mex3122020@gmail.com>
2022-04-11 12:49:13 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='EveryBotModulePack',
+    version='0.1b2',
+    description='Module package for EveryBot',
+    packages=['EveryBot'],
+    install_requires=['pyTelegramBotApi']
+)
